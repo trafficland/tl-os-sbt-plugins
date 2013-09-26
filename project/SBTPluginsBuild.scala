980995713214306946845aea446323ff24758d38
Eric D. Reichert <eric@reichertconsulting.com>
2013-09-26 11:59:52 -0400

    'Version bumped to 0.8.1-20130926-155952'

--- project/SBTPluginsBuild.scala
+++ project/SBTPluginsBuild.scala
@@ -12,7 +12,7 @@ object SBTPluginsBuild extends Build {
       organization := "com.trafficland",
       organizationName := "TrafficLand, Inc.",
       sbtPlugin := true,
-      version       := "0.8.0".toReleaseFormat,
+      version       := "0.8.1-SNAPSHOT".toReleaseFormat,
       scalaVersion := "2.9.2",
       scalacOptions := Seq("-deprecation", "-encoding", "utf8"),
       resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
