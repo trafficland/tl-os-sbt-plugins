5d14d215cf2bdf8beeb1b44be25e230ed755170d
Eric D. Reichert <eric@reichertconsulting.com>
2015-10-14 21:42:10 -0400

    Updated off the correct branch of the source repo.

--- src/main/scala/trafficland/opensource/sbt/plugins/play20/Play20Plugin.scala
+++ src/main/scala/trafficland/opensource/sbt/plugins/play20/Play20Plugin.scala
@@ -1,5 +1,6 @@
 package trafficland.opensource.sbt.plugins.play20
 
+import com.typesafe.sbt.web.Import
 import sbt._
 import sbt.Keys._
 import trafficland.opensource.sbt.plugins.distribute.DistributePlugin
@@ -11,22 +12,23 @@ object Play20Plugin extends Plugin {
   lazy val plug = DistributePlugin.plug ++ Seq(
     startScriptMainClass := "play.core.server.NettyServer",
     startScriptMainArguments := Seq("$basedir"),
-    additionalMappings <<= baseDirectory map { bd =>
-      bd / "public" match {
-        case p if !p.exists() => Seq.empty[(File, String)]
-        case public =>
-          addDirectory(public, List.empty)
+    additionalMappings <<= (unmanagedResourceDirectories in Import.Assets) map {
+      _.flatMap {
+        case dir if dir.exists() => addDirectory(dir)
+        case _ ⇒ Seq.empty[(File, String)]
       }
     }
   )
 
-  private def addDirectory(dir: File, accumulator: List[(File, String)]): Seq[(File, String)] = {
-    dir.listFiles().map {
-      case f @ file if !file.isDirectory =>
-        val relative = f.relativeTo(dir.getParentFile).map(r => r.getPath).get
-        (f, relative) :: accumulator
-      case _ @ d => addDirectory(d, accumulator)
-    }.flatMap { r => r}
+  private def addDirectory(dir: File): Seq[(File, String)] = {
+    def addFiles(td: File): Seq[(File, String)] = {
+      td.listFiles().map {
+        case f if f.isFile =>
+          val relative = f.relativeTo(dir.getParentFile).get.getPath
+          Seq((f, relative))
+        case d if d.isDirectory => addFiles(d)
+      }.flatten.toSeq
+    }
+    addFiles(dir)
   }
-
 }
